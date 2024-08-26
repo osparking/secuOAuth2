63aff4751b8cbfeee81dcc6bfa92aa44e04aa899
osparking <jbpark03@gmail.com>
2024-08-26 17:08:08 +0900

    only social login - initial

---
+++ src/main/java/com/bumsoap/secuOAuth2/SecuOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.bumsoap.secuOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuOAuth2Application.class, args);
+	}
+
+}
