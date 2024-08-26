9e33c9d63d17a9fb245b310b68245dec8de2ccd3
osparking <jbpark03@gmail.com>
2024-08-26 20:23:00 +0900

    default security filter chain - oAuth 2 login enabled

---
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bumsoap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity)
+        throws  Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/secured").authenticated()
+                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
