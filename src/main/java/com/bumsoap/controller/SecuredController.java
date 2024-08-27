73d6ab5c9a26b767f028a066969f1c7c9ccced50
osparking <jbpark03@gmail.com>
2024-08-27 11:19:24 +0900

    print the authN method actually used

--- src/main/java/com/bumsoap/controller/SecuredController.java
+++ src/main/java/com/bumsoap/controller/SecuredController.java
@@ -1,12 +1,20 @@
 package com.bumsoap.controller;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SecuredController {
     @GetMapping("secured")
-    public String securePage() {
+    public String securePage(Authentication authentication) {
+        if (authentication instanceof UsernamePasswordAuthenticationToken userPassToken) {
+            System.out.println(userPassToken);
+        } else if (authentication instanceof OAuth2AuthenticationToken oAuth2Token) {
+            System.out.println(oAuth2Token);
+        }
         return "secured.html";
     }
 }
